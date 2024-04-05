936ebb77866b1c277cd0640638b39d9b0b0d425b
Bruno José <bru.matias100@gmail.com>
2024-04-05 16:14:27 -0300

    feat: event table created

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import fastify from 'fastify'
+import { z } from 'zod'
 
 const app = fastify()
+
+app.post('/events', (request, reply) => {
+
+  return 'Hello'
+})
+
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running')
